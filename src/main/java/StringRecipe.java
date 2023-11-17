7a58e8778a9fd11f49bdaa4bc1c9070aba8ce6f9
YaBoiKazz <tomastelmo10@gmail.com>
2023-11-17 11:24:30 +0000

    1 feito

---
+++ src/main/java/StringRecipe.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class StringRecipe {
+    List<StringTransformer>transformers;
+    public StringRecipe(List<StringTransformer> transformers){
+        this.transformers = transformers;
+    }
+
+    public void mix(StringDrink drink){
+        for(int i =0; i < transformers.size(); i++){
+                transformers.get(i).execute(drink);
+        }
+    }
+}
